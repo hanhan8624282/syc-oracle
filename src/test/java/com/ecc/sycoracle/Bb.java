e1419c6ddcb489562abef26bbbd61e1639d65a0f
sunchao <3412786980@qq.com>
2022-08-01 15:03:46 +0800

    增加sunchao.java测试文件变更--bb1

--- src/test/java/com/ecc/sycoracle/Bb.java
+++ src/test/java/com/ecc/sycoracle/Bb.java
@@ -1,11 +1,11 @@
 package com.ecc.sycoracle;
 
-/**
- * @author sunyc
- * @create 2022-08-01 15:01
- */
+
 public class Bb {
     public static void main(String[] args) {
         System.out.println("zheli   BBBBBB");
+        System.out.println("zheli   BBBBBB");
+        System.out.println("zheli   BBBBBB");
+
     }
 }

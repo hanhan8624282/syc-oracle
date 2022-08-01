c192e762224d7118fee78ba1cc121d303ebbc9f7
sunchao <3412786980@qq.com>
2022-08-01 15:02:47 +0800

    增加sunchao.java测试文件变更--master6

--- src/test/java/com/ecc/sycoracle/Bb.java
+++ src/test/java/com/ecc/sycoracle/Bb.java
@@ -6,6 +6,9 @@ package com.ecc.sycoracle;
  */
 public class Bb {
     public static void main(String[] args) {
+
         System.out.println("zheli   BBBBBB");
+        System.out.println("zheli   master");
+
     }
 }

1bd75af592e5fd94bd24c669e6a6ff5dc9696f42
sunchao <3412786980@qq.com>
2022-08-01 14:39:32 +0800

    增加sunchao.java测试文件

---
+++ src/test/java/com/ecc/sycoracle/SunChao.java
@@ -0,0 +1,14 @@
+package com.ecc.sycoracle;
+
+/**
+ * @author sunyc
+ * @create 2022-08-01 14:38
+ */
+public class SunChao {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("123");
+    }
+}
